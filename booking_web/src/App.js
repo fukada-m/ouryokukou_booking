a220aa21fc0fef2340174997e4e04327e8de0ce0
fukada-m <fukada.m8888@gmail.com>
2023-07-04 10:16:55 +0900

    とりあえずapiを作った

--- booking_web/src/App.js
+++ booking_web/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { axiosInstance } from './utils/axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [status, setStatus] = useState('チェックしてみて');
+
+  const onClick = async () => {
+    const res =  await axiosInstance.get("/health_check");
+    setStatus(res.data.status);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: "auto", width: "1000px" }}>
+      <h1>Booking Web</h1>
+      <div style={ {marginTop: "20px"} }>
+            <p>Status: {status}</p>
+        <button onClick={onClick}>Health Check</button>
+      </div>
     </div>
   );
 }
