715a812f479520448f3a478f95414c5f685b0c35
fukada-m <fukada.m8888@gmail.com>
2023-07-06 16:58:35 +0900

    corsをいじった

--- booking_api/config/initializers/cors.rb
+++ booking_api/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://booking-web.yunonn.net", "http://booking-web.yunonn.net"
+    origins "http://localhost:3000", "https://booking.yunonn.net"
 
     resource "*",
       headers: :any,
