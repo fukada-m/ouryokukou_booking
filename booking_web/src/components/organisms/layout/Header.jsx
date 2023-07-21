acc1114dfa3432788197c76c055e0c941e0e5772
fukada-m <fukada.m8888@gmail.com>
2023-07-21 22:34:04 +0900

    ヘッダーにリンクをつけた

--- booking_web/src/components/organisms/layout/Header.jsx
+++ booking_web/src/components/organisms/layout/Header.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Flex, Button, Heading, Link, Box, useDisclosure } from "@chakra-ui/react";
 import { MenuIconButton } from "../../atoms/button/MenuIconButton";
 import { MenuDrawer } from "../../molecules/MenuDrawer";
 import { Options } from "../../molecules/Options";
+import { useNavigate } from "react-router-dom";
 export const Header = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const navigate = useNavigate();
+
+  const onClickAllBooking = useCallback(() => navigate("/allBooking"), []);
+  const onClickTodayBooking = useCallback(() => navigate("/todayBooking"), []);
+  const onClickCreateBooking = useCallback(() => navigate("/createBooking"), []);
+
   return (
     <>
       <Flex
@@ -20,25 +27,17 @@ export const Header = () => {
         </Heading>
         <Flex align="center" flexGrow={2}>
           <Box pr={4} pl={4}>
-            <Link>全ての予約</Link>
+            <Link onClick={onClickAllBooking}>全ての予約</Link>
           </Box>
           <Box pr={4}>
-            <Link>今日の予約</Link>
+            <Link onClick={onClickTodayBooking}>今日の予約</Link>
           </Box>
-          <Link>新規予約</Link>
+          <Link onClick={onClickCreateBooking}>新規予約</Link>
         </Flex>
         <Options />
         <MenuIconButton onOpen={onOpen} />
       </Flex>
       <MenuDrawer onClose={onClose} isOpen={isOpen} />
     </>
-    // <div>
-    //   <h1>Header</h1>
-    //   <Link to="/allBooking">全ての予約を表示</Link>
-    //   <span> | </span>
-    //   <Link to="/todayBooking">今日の予約を表示</Link>
-    //   <span> | </span>
-    //   <Link to="/createBooking">新規予約を作成</Link>
-    // </div>
   );
 };
