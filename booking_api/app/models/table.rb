7f5210e165733cc17d743c84b9b0cd2adc9f2c41
fukada-m <fukada.m8888@gmail.com>
2023-08-03 18:00:56 +0900

    ロボコップとCI/CD

--- booking_api/app/models/table.rb
+++ booking_api/app/models/table.rb
@@ -4,5 +4,5 @@ class Table < ApplicationRecord
   has_and_belongs_to_many :bookings
 
   validates :name, presence: true, uniqueness: true,
-  inclusion: { in: %w[1番 2番 3番 5番 11番 12番 13番 14番 15番 16番 21番] }
+                   inclusion: { in: %w[1番 2番 3番 5番 11番 12番 13番 14番 15番 16番 21番] }
 end
