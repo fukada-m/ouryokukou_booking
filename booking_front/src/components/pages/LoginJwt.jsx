6f24afcba68024d0d7ed1caddf0cd9f627db90f5
fukada-m <fukada.m8888@gmail.com>
2023-08-07 17:40:18 +0900

    ペラ１を埋め込み

--- booking_front/src/components/pages/LoginJwt.jsx
+++ booking_front/src/components/pages/LoginJwt.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import { login } from "../../utils/api";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, Image } from "@chakra-ui/react";
 import { BaseInput } from "../atoms/input/BaseInput";
 import { MainHeading } from "../atoms/heading/MainHeading";
 import { useMessage } from "../../hooks/useMessage";
+import app_describe from "../../images/app_describe.png";
 
 export const LoginJwt = () => {
   const [email, setEmail] = useState("");
@@ -25,34 +26,37 @@ export const LoginJwt = () => {
   };
 
   return (
-    <Box m="auto" p={4} w={300} bg={"pink"}>
-      <MainHeading>ようこそ</MainHeading>
-      <label>
-        Email:
-        <BaseInput
-          w="250px"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </label>
-      <label>
-        Password:
-        <BaseInput
-          w="250px"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </label>
-      <Button my={3} onClick={handleSubmit}>
-        ログイン
-      </Button>
-      <p>email: yakiniku@example.com </p>
-      <p>password: pass</p>
-      <p>ご自由にお試しください</p>
-    </Box>
+    <>
+      <Box m="auto" my={5} p={4} w={300} bg={"pink"}>
+        <MainHeading>ようこそ</MainHeading>
+        <label>
+          Email:
+          <BaseInput
+            w="250px"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </label>
+        <label>
+          Password:
+          <BaseInput
+            w="250px"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+        <Button my={3} onClick={handleSubmit}>
+          ログイン
+        </Button>
+        <p>email: yakiniku@example.com </p>
+        <p>password: pass</p>
+        <p>ご自由にお試しください</p>
+      </Box>
+      <Image m={"auto"} src={app_describe} alt="アプリの説明" />
+    </>
   );
 };
